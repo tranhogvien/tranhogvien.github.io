f8ea3e26e1abba8c1476255953461fff66c5eced
tranhongdiep96.96@gmail.com <tranhongdiep96.96@gmail.com>
2024-08-26 16:00:42 +0700

    add proj

---
+++ src/main.js
@@ -0,0 +1,8 @@
+GetPortfolioData()
+document.getElementById("portfolio").style.display = "block";
+// loading.style.display = "block";
+// loading.style.backgroundColor = 'rgba(30, 30, 30, 1)';
+// loading.style.display = "none";
+setTimeout(() => {
+    document.getElementById("loading-progress").style.display = "none";
+}, 800);
